4b92ade5c0a926f670f3df5ca303d52555847aad
Abdoulhakim89 <abdoulbeast89@gmail.com>
2025-06-30 15:35:00 +0200

    Update solution - CodeHub

--- 6-kyu/split-and-then-add-both-sides-of-an-array-together/split-and-then-add-both-sides-of-an-array-together.js
+++ 6-kyu/split-and-then-add-both-sides-of-an-array-together/split-and-then-add-both-sides-of-an-array-together.js
@@ -1,13 +1,9 @@
-function splitAndAdd (arr,n){
-    if(!n) return arr
-    let arr1 = arr.slice(0, Math.floor(arr.length/2)).reverse()
-    let arr2 = arr.slice(arr1.length).reverse()
-    let added = arr2.map((a, i) => a+(arr1[i] || 0)).reverse()
-    // console.log(arr)
-    // console.log(added)
-    if(n > 1 ) return splitAndAdd(arr=added, n-1)
-    return added
+const splitAndAdd = (arr,n) => {
+    if(!n) return arr;
+    let arr1 = arr.slice(0, Math.floor(arr.length/2)).reverse();
+    let arr2 = arr.slice(arr1.length).reverse();
+    let added = arr2.map((a, i) => a+(arr1[i] || 0)).reverse();
     
-    
+    return n > 1? splitAndAdd(arr=added, n-1): added
     
 }
